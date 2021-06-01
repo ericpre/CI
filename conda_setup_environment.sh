054d7bb867db2ffba15bc1a9683120fac7e5e796
Eric Prestat <eric.prestat@gmail.com>
2021-06-01 23:08:06 +0100

    Use Mambaforge

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,8 +1,6 @@
 ENV_NAME="test_env"
 
 conda config --set channel_priority strict
-conda update -y conda -c conda-forge
-conda install -y mamba -c conda-forge
 mamba create -y --name $ENV_NAME python=$PYTHON_VERSION
 mamba env update --file conda_environment.yml
 mamba env update --file conda_environment_dev.yml
