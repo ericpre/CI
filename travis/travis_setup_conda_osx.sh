12ab0ddf653c59e67ee8df35a9065e6d55e29b69
Eric Prestat <eric.prestat@gmail.com>
2021-06-01 20:50:25 +0100

    Use mambaforge on travis.

--- travis/travis_setup_conda_osx.sh
+++ travis/travis_setup_conda_osx.sh
@@ -4,6 +4,6 @@ sudo Xvfb :99 -ac -screen 0 1024x768x8 &
 sleep 1 # give xvfb some time to start
 
 # Setup conda
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
+curl -fsSLo mambaforge.sh https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-$(uname -m).sh
+bash mambaforge.sh -b -p $HOME/mambaforge
 
