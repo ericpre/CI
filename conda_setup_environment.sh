b73cbcdd543228e347bc39542ca6c207c5e9f4c9
Eric Prestat <eric.prestat@gmail.com>
2021-06-01 21:14:06 +0100

    Explicitly add "nodefaults" to environment file

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,8 +1,8 @@
 ENV_NAME="test_env"
 
 conda update -y conda
-mamba env update --name $ENV_NAME python=$PYTHON_VERSION
-mamba env update --name $ENV_NAME --file conda_environment.yml
-mamba env update --name $ENV_NAME --file conda_environment_dev.yml
+mamba create --name $ENV_NAME python=$PYTHON_VERSION
+mamba env update --file conda_environment.yml
+mamba env update --file conda_environment_dev.yml
 conda info
 conda list -n $ENV_NAME
