7944854680ce93686ce4483bfd02633f835ed646
Eric Prestat <eric.prestat@gmail.com>
2021-06-01 20:50:04 +0100

    Use mamba in azure pipeline.

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,8 +1,8 @@
 ENV_NAME="test_env"
 
 conda update -y conda
-conda create --yes --quiet --name $ENV_NAME python=$PYTHON_VERSION
-conda env update --name $ENV_NAME --file conda_environment.yml
-conda env update --name $ENV_NAME --file conda_environment_dev.yml
+mamba env update --name $ENV_NAME python=$PYTHON_VERSION
+mamba env update --name $ENV_NAME --file conda_environment.yml
+mamba env update --name $ENV_NAME --file conda_environment_dev.yml
 conda info
 conda list -n $ENV_NAME
