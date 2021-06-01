3b6de9c46821511056e150a16903f0cf3b700dc9
Eric Prestat <eric.prestat@gmail.com>
2021-06-01 21:32:53 +0100

    Don't use main branch...

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,7 +1,9 @@
 ENV_NAME="test_env"
 
-conda update -y conda
-mamba create --name $ENV_NAME python=$PYTHON_VERSION
+conda config --set channel_priority strict
+conda update -y conda -c conda-forge
+conda install -y mamba -c conda-forge
+mamba create -y --name $ENV_NAME python=$PYTHON_VERSION
 mamba env update --file conda_environment.yml
 mamba env update --file conda_environment_dev.yml
 conda info
